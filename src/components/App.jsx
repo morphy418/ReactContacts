cca28bf5aa24a50a85f20e87535ca89766ba6114
Zoltan Harshegyi <66206564+morphy418@users.noreply.github.com>
2021-02-01 16:34:26 +0000

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Card from "./Card"
+import contacts from "../contacts";
+
+function createCard(contact) {
+  return <Card 
+    id={contact.id}
+    key={contact.id}
+    name={contact.name}
+    img={contact.imgURL}
+    tel={contact.phone}
+    email={contact.email}
+  />
+}
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      {contacts.map(createCard)}
+
+      {/* <Card 
+        name="Beyonce"
+        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        tel="+123 456 789"
+        email="b@beyonce.com"
+      /> */}
+      {/* <Card 
+        name= {contacts[0].name}
+        img= {contacts[0].imgURL}
+        tel= {contacts[0].phone}
+        email= {contacts[0].email}
+      />
+      <Card 
+        name= {contacts[1].name}
+        img= {contacts[1].imgURL}
+        tel= {contacts[1].phone}
+        email= {contacts[1].email}
+      />
+      <Card 
+        name= {contacts[2].name}
+        img= {contacts[2].imgURL}
+        tel= {contacts[2].phone}
+        email= {contacts[2].email}
+      /> */}
+    </div>
+  );
+}
+
+
+export default App;
